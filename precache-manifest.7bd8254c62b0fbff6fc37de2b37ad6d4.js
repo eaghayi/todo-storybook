8fffe6740d9e0f22963b86ca596289a391c78529
Ankit Roy <ankit@Ankits-MacBook-Pro.local>
2020-06-18 12:59:40 -0400

    Updates

---
+++ precache-manifest.7bd8254c62b0fbff6fc37de2b37ad6d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d652263ed9175ecf1bd09b8bffda89fb",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "4f82e336f0191ecffd23",
+    "url": "/ToDoApp/static/css/main.ea6538c8.chunk.css"
+  },
+  {
+    "revision": "37ff28e106287fb85d07",
+    "url": "/ToDoApp/static/js/2.c6817ddf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoApp/static/js/2.c6817ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f82e336f0191ecffd23",
+    "url": "/ToDoApp/static/js/main.984f43b6.chunk.js"
+  },
+  {
+    "revision": "7f224c52611e29f5f6ad",
+    "url": "/ToDoApp/static/js/runtime-main.f5190181.js"
+  }
+]);
